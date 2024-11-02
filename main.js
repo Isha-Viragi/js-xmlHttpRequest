b3ec543603ba8c560e46d17638661f79ea5e959c
Isha Viragi <isha.viragi@gmail.com>
2024-11-02 12:39:08 -0400

    Create new xmlHttpRequest and display 3 facts on page

--- main.js
+++ main.js
@@ -1,11 +1,25 @@
-const xhr = new XMLHttpRequest(); //Create new object from built-in class
+//First Request
+const xhr1 = new XMLHttpRequest(); //Create new object from built-in class
 
-xhr.open('GET', "https://meowfacts.herokuapp.com/") //Set up get request
-xhr.send() //Send the request
+xhr1.open('GET', "https://meowfacts.herokuapp.com/") //Set up get request
+xhr1.send() //Send the request
 
-xhr.addEventListener('load', () => { //Listen for response
-  const data = JSON.parse(xhr.response).data;
-  console.log(data)
+xhr1.addEventListener('load', () => { //Listen for response
+  const data = JSON.parse(xhr1.response).data; //Data passed via http will always be JSON
+  document.querySelector(".js-display-1").innerHTML = data
+})
 
-  document.querySelector(".js-display").innerHTML = data
-}) 
+
+
+//Second Request
+const xhr2 = new XMLHttpRequest();
+xhr2.open('GET', "https://meowfacts.herokuapp.com/?count=3"); //Set up the request
+xhr2.send(); //Send the request
+
+xhr2.addEventListener('load', () => { //Listen for response
+  const data = JSON.parse(xhr2.response).data //Data passed via http will always be JSON
+  data.forEach(fact => {
+    document.querySelector(".js-display-2").innerHTML += fact
+  });
+})
+
