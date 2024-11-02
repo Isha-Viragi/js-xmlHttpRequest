bb8b1156617eaaa3535bfdab54fe700917a36379
Isha Viragi <isha.viragi@gmail.com>
2024-11-02 18:12:10 -0400

    Refactor metObjectIdRequest code into exportable function that takes a word to find id

--- metObjectIdRequest.js
+++ metObjectIdRequest.js
@@ -1,13 +1,13 @@
-const xhr = new XMLHttpRequest();
+export function metObjectIdRequest(word) {
+  const xhr = new XMLHttpRequest();
 
-xhr.open('GET', "https://collectionapi.metmuseum.org/public/collection/v1/search?q=maine");
-xhr.send();
+  xhr.open('GET', `https://collectionapi.metmuseum.org/public/collection/v1/search?q=${word}`);
+  xhr.send();
 
-xhr.addEventListener('load', () => {
-  const response = JSON.parse(xhr.response).objectIDs[0];
-  console.log(response);
-
-})
-
+  xhr.addEventListener('load', () => {
+    const response = JSON.parse(xhr.response).objectIDs[0];
+    console.log(response);
+  })
+}
 
 
