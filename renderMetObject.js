a7659dc72abdde60da104906ecd28f669eb860f2
Isha Viragi <isha.viragi@gmail.com>
2024-11-08 18:22:05 -0500

    Conditionally render image from met object response

--- renderMetObject.js
+++ renderMetObject.js
@@ -3,9 +3,12 @@ export function renderMetObject(data) {
   let html;
   html = `
   <div>${data.title || "No title found"}</div>
-  <div>${data.primaryImage || "No image found"}</div>
+  <div>
+  ${(data.primaryImage && `<img src=${data.primaryImage}>`) || "No image found"}
+  </div>
   <div>${data.objectName || "No object name found"}</div>
   <div>${data.artistDisplayName || "No artist name found"}</div>
   `
   display.innerHTML = html || "Could not find a MET object";
+  console.log(data.primaryImage)
 }
